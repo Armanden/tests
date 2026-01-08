01226997f9ec80f05f77efeb3e663d078c9ebe1a
jesterknightofficial <jesterknight@tutamail.com>
2026-01-08 23:30:43 +0300

    according to github html is a language

--- random.c
+++ random.c
@@ -7,12 +7,20 @@
 const char *g[] = {"up", "down", "left", "right"}; 
 
 int main() {
-    Color green = {20, 160, 133, 255};
+    Color colors[] = {
+        {20, 160, 133, 255}, // Original green
+        {255, 0, 0, 255},   // Red
+        {0, 255, 0, 255},   // Green
+        {0, 0, 255, 255},   // Blue
+        {255, 255, 0, 255}, // Yellow
+        {255, 165, 0, 255}  // Orange
+    };
+    
     int ballX = 400;
     int ballY = 400;
-    int speed = 5; // Ball movement speed
-    const int ballRadius = 20; // Radius of the ball
-    InitWindow(800, 800, "Random Ball");
+    int speed = 5; 
+    const int ballRadius = 20; 
+    InitWindow(800, 800, "Disco Ball");
     SetTargetFPS(60);
 
     srand((unsigned int)time(NULL));
@@ -21,17 +29,19 @@ int main() {
     float changeDirectionTimer = 0.0f; 
     const float changeInterval = 1.0f;  
 
+    Color currentBackgroundColor = colors[rand() % (sizeof(colors) / sizeof(colors[0]))]; 
+
     while (!WindowShouldClose()) {
         // Update the timer
         changeDirectionTimer += GetFrameTime();
 
-        // Check if it's time to change direction
+
         if (changeDirectionTimer >= changeInterval) {
-            c = g[rand() % (sizeof(g) / sizeof(g[0]))]; // Choose a new direction
-            changeDirectionTimer = 0.0f; // Reset timer
+            c = g[rand() % (sizeof(g) / sizeof(g[0]))];
+            currentBackgroundColor = colors[rand() % (sizeof(colors) / sizeof(colors[0]))]; 
+            changeDirectionTimer = 0.0f; 
         }
 
-        // Move the ball in the current direction with boundary checks
         if (strcmp(c, "right") == 0) {
             if (ballX + speed + ballRadius <= GetScreenWidth()) {
                 ballX += speed;
@@ -50,14 +60,13 @@ int main() {
             }
         }
 
-        // Draw the ball
         BeginDrawing();
-        ClearBackground(green);
+        ClearBackground(currentBackgroundColor); 
         DrawCircle(ballX, ballY, ballRadius, WHITE);
         EndDrawing();
     }
 
-    CloseWindow(); // Close the window cleanly
+    CloseWindow(); 
     return 0;
 }
 
